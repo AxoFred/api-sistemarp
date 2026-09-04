ef28369669c8dca163a2aad30c85cedc0a9ce65b
Jesus Eduardo Flores de Jesus <eduardo.flores131003@gmail.com>
2026-09-03 18:32:43 -0600

    Rutas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AlfabetizatecController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\UsuariosController;
@@ -7,7 +8,10 @@ use App\Http\Controllers\API\CarreraController;
 use App\Http\Controllers\API\MatriculaController;
 use App\Http\Controllers\API\ServicioSocialController;
 use App\Http\Controllers\API\ConvenioController;
-
+use App\Http\Controllers\API\EducandoController;
+use App\Http\Controllers\API\NivelEducandoController;
+use App\Http\Controllers\API\ResidenciaController;
+use App\Http\Controllers\API\VisitaIndustrialController;
 
 /*
 |--------------------------------------------------------------------------
@@ -70,5 +74,39 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/servicio-social/{id}', [ServicioSocialController::class, 'show']);
     Route::put('/servicio-social/{id}', [ServicioSocialController::class, 'update']);
     Route::delete('/servicio-social/{id}', [ServicioSocialController::class, 'destroy']);
-   
+    
+    // residencias
+    Route::get('/residencias', [ResidenciaController::class, 'index']);
+    Route::post('/residencias', [ResidenciaController::class, 'store']);
+    Route::get('/residencias/{id}', [ResidenciaController::class, 'show']);
+    Route::put('/residencias/{id}', [ResidenciaController::class, 'update']);
+    Route::delete('/residencias/{id}', [ResidenciaController::class, 'destroy']);
+
+    // visitas industriales
+    Route::get('/visitas-industriales', [VisitaIndustrialController::class, 'index']);
+    Route::post('/visitas-industriales', [VisitaIndustrialController::class, 'store']);
+    Route::get('/visitas-industriales/{id}', [VisitaIndustrialController::class, 'show']);
+    Route::put('/visitas-industriales/{id}', [VisitaIndustrialController::class, 'update']);
+    Route::delete('/visitas-industriales/{id}', [VisitaIndustrialController::class, 'destroy']);
+
+    // niveles educando
+    Route::get('/niveles-educando', [NivelEducandoController::class, 'index']);
+    Route::post('/niveles-educando', [NivelEducandoController::class, 'store']);
+    Route::get('/niveles-educando/{id}', [NivelEducandoController::class, 'show']);
+    Route::put('/niveles-educando/{id}', [NivelEducandoController::class, 'update']);
+    Route::delete('/niveles-educando/{id}', [NivelEducandoController::class, 'destroy']);
+
+    // educandos
+    Route::get('/educandos', [EducandoController::class, 'index']);
+    Route::post('/educandos', [EducandoController::class, 'store']);
+    Route::get('/educandos/{id}', [EducandoController::class, 'show']);
+    Route::put('/educandos/{id}', [EducandoController::class, 'update']);
+    Route::delete('/educandos/{id}', [EducandoController::class, 'destroy']);
+
+    // alfabetizatec
+    Route::get('/alfabetizatec', [AlfabetizatecController::class, 'index']);
+    Route::post('/alfabetizatec', [AlfabetizatecController::class, 'store']);
+    Route::get('/alfabetizatec/{id}', [AlfabetizatecController::class, 'show']);
+    Route::put('/alfabetizatec/{id}', [AlfabetizatecController::class, 'update']);
+    Route::delete('/alfabetizatec/{id}', [AlfabetizatecController::class, 'destroy']);
 });
